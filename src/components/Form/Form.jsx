b950776e8a6a703df99db22d820e910b5847e2c9
thaylla <thayllachristineo@gmail.com>
2024-03-11 20:48:05 -0300

    feat: implements Dropdown and Button into Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,39 @@
 import './Form.css';
 
 import Input from '../Input';
+import Dropdown from '../Dropdown';
+import Button from '../Button';
 
-const Form = () => (
-  <section>
-    <form>
-      <h2>Preencha os dados para criar o card do colaborador</h2>
-      <Input label="Nome" placeholder="Digite seu nome" />
-      <Input label="Cargo" placeholder="Digite seu cargo" />
-      <Input label="Imagem" placeholder="Informe o endereço da imagem" />
-      <Input label="Time" placeholder="Digite seu nome" />
-    </form>
-  </section>
-);
+const Form = () => {
+  const times = [
+    ' ',
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'DevOps',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão',
+  ];
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    console.log('O form foi submetido!');
+  };
+
+  return (
+    <section>
+      <form onSubmit={onSubmit}>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <Input label="Nome" placeholder="Digite seu nome" />
+        <Input label="Cargo" placeholder="Digite seu cargo" />
+        <Input label="Imagem" placeholder="Informe o endereço da imagem" />
+        <Dropdown label="Time" options={times} />
+
+        <Button>Criar card</Button>
+      </form>
+    </section>
+  );
+};
 
 export default Form;
